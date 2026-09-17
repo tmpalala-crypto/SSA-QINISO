1e1000e7d22d61033cf98735cb54e3b651872e38
tmpalala-crypto <tmpalala-crypto@users.noreply.github.com>
2026-09-17 21:35:24 +0000

    deploy: 4d9d69e03da27356ad4943ce1708b57f743a51d0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"06a2e2a110089dff50fe635cffd2a61e1b24fb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3287253232" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1244000901" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
